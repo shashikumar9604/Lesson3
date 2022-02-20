9988f10448506e484792e56a28916fd2cb9d6be2
Admin <Admin@DESKTOP-JDAQI98>
2022-02-20 10:35:42 +0530

    Phase-2 Assisted Program

---
+++ MyWebApp/src/main/java/com/shashi/demo/HomeController.java
@@ -0,0 +1,17 @@
+package com.shashi.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class HomeController 
+{
+	@RequestMapping("home")
+ public String home()
+ {
+	 System.out.println("Hi shashi");
+	 return "home.jsp";
+ }
+ 
+}
